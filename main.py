11c54eaf49340a7b3b19d79ca1ebf16f82954499
chxy75 <160509015+chxy75@users.noreply.github.com>
2024-04-08 13:58:36 +0000

    final

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # Part 1
+from io import FileIO
+
+
 def read_csv(filename):
-  '''seperate the data'''
+  """
+  read data from a csv file and edit it
+
+  Parameters
+  ----------
+  filename:csv
+      the file to be read
+
+  Returns
+  -------
+  list
+  """
   # Type your code below
   filename = open(filename, 'r')
   header = filename.readline().strip('\n').split(',')
@@ -22,7 +36,21 @@ def read_csv(filename):
 
 
 # Part 2
-def filter_gender(enrolment_by_age, sex):
+def filter_gender(enrolment_by_age:list, sex:str) -> list:
+  """
+  filter the data
+
+  Parameters
+  ----------
+  enrolment_by_age:list
+      list of records
+  sex:str
+      the str to be matched with
+
+  Returns
+  -------
+  list
+  """
   new_data = []
   for record in enrolment_by_age:
       if record[2] == sex:
@@ -32,8 +60,20 @@ def filter_gender(enrolment_by_age, sex):
 
 
 # Part 3
-def sum_by_year(enrolment):
+def sum_by_year(enrolment:list)->list:
   # Type your code below
+  """
+  sum up emrolment by year
+
+  Parameters
+  ----------
+  enrolment:list
+      list of data
+
+  Returns
+  -------
+  list
+  """
   enrolment_by_year = []
   new_row = [None, None]
   for record in enrolment:
@@ -48,16 +88,34 @@ def sum_by_year(enrolment):
 
 
 # Part 4
-def write_csv(filename, header, data):
+def write_csv(filename, header:str, data:list):
   # Type your code below
-    file = open(filename, 'w')
-    file.write(str(header)/n)
+  """
+  write data into a csv file
 
-    for row in data:
-      file.write(str(data)/n)
+  Parameters
+  ----------
+  filename:
+    file to be written into
+  header:list
+    str to be header
+  data:list
+    ist of data to be written
 
-    num_lines_written = len(data) + 1  
-    return num_lines_written
+  Returns
+  -------
+  file
+  """
+  file = open(filename, 'w')
+  header = str(header)
+  file.write(header + '/n')
+
+  for row in data:
+      row = str(row)
+      file.write(row + '/n')
+
+  num_lines_written = len(data) + 1  
+  return num_lines_written
 
 
 
